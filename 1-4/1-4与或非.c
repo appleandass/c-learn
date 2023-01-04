6ece40dacaa48a2ff2e7fa8186e3922e7dbf7481
appleandass <mistakenotfound404error@gmail.com>
2023-01-04 18:35:56 +0800

    补充提交

---
+++ 1-4/1-4与或非.c
@@ -0,0 +1,17 @@
+/*#include <stdio.h>
+//逻辑运算 ! &&都真为真 ||有一个为真结果就为真
+int main() {
+	// !>&&>||
+	//!done&&(count>MAX)
+	//条件运算符 ? ; 三目运算符
+
+	return 0;
+}*/
+#include <stdio.h>
+
+int main() {
+	//switch-case 需要int类型的表达式
+
+	return 0;
+}
+
